faa3f354078728434e52f576f09109bf04d83a63
herocat <1722791503@qq.com>
2023-09-01 09:36:56 +0800

    update drop module

--- test/test_octree_drop.py
+++ test/test_octree_drop.py
@@ -15,28 +15,28 @@ from ..modules.octree_drop import HextreeDropPath
 
 class HextreeDropTest(unittest.TestCase):
 
-  def test_hextree_drop_path(self):
-    r'''Just execute the `OctreeDropPath`, and there are no comparisons with 
-    ground-truth results.
-    '''
+    def test_hextree_drop_path(self):
+        r'''Just execute the `OctreeDropPath`, and there are no comparisons with 
+        ground-truth results.
+        '''
 
-    hextrees = [get_hextree(i) for i in ([4] * 8 + [5] * 2)]
-    hextree = hextree.merge_hextrees(hextrees)
+        hextrees = [get_hextree(i) for i in ([4] * 8 + [5] * 2)]
+        hextree = hextree.merge_hextrees(hextrees)
 
-    # Test 1
-    depth = 5
-    nnum = hextree.nnum[depth]
-    data = torch.rand(nnum, 4) # TODO 3 or 4
-    drop_path = HextreeDropPath(drop_prob=0.8, nempty=False)
-    output = drop_path(data, hextree, depth)
+        # Test 1
+        depth = 5
+        nnum = hextree.nnum[depth]
+        data = torch.rand(nnum, 4)  # TODO 3 or 4
+        drop_path = HextreeDropPath(drop_prob=0.8, nempty=False)
+        output = drop_path(data, hextree, depth)
 
-    # Test 2
-    nnum_nempty = hextree.nnum_nempty[depth]
-    data = torch.rand(nnum_nempty, 4) # TODO 3 or 4
-    drop_path = HextreeDropPath(drop_prob=0.8, nempty=True)
-    output = drop_path(data, hextree, depth)
+        # Test 2
+        nnum_nempty = hextree.nnum_nempty[depth]
+        data = torch.rand(nnum_nempty, 4)  # TODO 3 or 4
+        drop_path = HextreeDropPath(drop_prob=0.8, nempty=True)
+        output = drop_path(data, hextree, depth)
 
 
 if __name__ == "__main__":
-  os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-  unittest.main()
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+    unittest.main()
--- test/utils.py
+++ test/utils.py
@@ -9,31 +9,30 @@ import os
 import torch
 import numpy as np
 
-import ocnn
+# import ocnn
+import hextree as ht
 
 
 def get_points(id, return_data=False):
-  folder = os.path.dirname(__file__)
-  filename = os.path.join(folder, 'data/octree/test_%03d.npz' % id)
-  data = np.load(filename)
+    folder = os.path.dirname(__file__)
+    filename = os.path.join(folder, 'data/octree/test_%03d.npz' % id)
+    data = np.load(filename)
 
-  points, normals = data['points'], data['normals']
-  point_cloud = ocnn.octree.Points(
-      torch.from_numpy(points), torch.from_numpy(normals))
-  return (point_cloud, data) if return_data else point_cloud
-
-
-def get_octree(id, return_data=False):
-  point_cloud, data = get_points(id, return_data=True)
-  octree = ocnn.octree.Octree(
-      data['depth'].item(), full_depth=data['full_depth'].item())
-  octree.build_octree(point_cloud)
-  return (octree, data) if return_data else octree
+    points, normals = data['points'], data['normals']
+    point_cloud = ht.Points(torch.from_numpy(points), torch.from_numpy(normals))
+    return (point_cloud, data) if return_data else point_cloud
 
 
-def get_batch_octree(device='cpu'):
-  octree1 = get_octree(4).to(device)
-  octree2 = get_octree(5).to(device)
-  octree = ocnn.octree.merge_octrees([octree1, octree2])
-  octree.construct_all_neigh()
-  return octree
+def get_hextree(id, return_data=False):
+    point_cloud, data = get_points(id, return_data=True)
+    hextree = ht.Hextree(data['depth'].item(), full_depth=data['full_depth'].item())
+    hextree.build_hextree(point_cloud)
+    return (hextree, data) if return_data else hextree
+
+
+def get_batch_hextree(device='cpu'):
+    hextree1 = get_hextree(4).to(device)
+    hextree2 = get_hextree(5).to(device)
+    hextree = ht.merge_hextrees([hextree1, hextree2])
+    hextree.construct_all_neigh()
+    return hextree
