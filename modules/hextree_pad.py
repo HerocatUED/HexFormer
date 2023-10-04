c136ed9b3e56f62cf57a59c2fc8488f896b9d301
herocat <1722791503@qq.com>
2023-10-04 09:51:16 +0800

    code format

--- modules/hextree_pad.py
+++ modules/hextree_pad.py
@@ -13,29 +13,29 @@ from hextree import Hextree
 
 
 def hextree_pad(data: torch.Tensor, hextree: Hextree, depth: int, val: float = 0.0):
-  r''' Pads :attr:`val` to make the number of elements of :attr:`data` equal to
-  the hextree node number.
+    r''' Pads :attr:`val` to make the number of elements of :attr:`data` equal to
+    the hextree node number.
 
-  Args:
-    data (torch.Tensor): The input tensor with its number of elements equal to the
-        non-empty hextree node number.
-    hextree (Hextree): The corresponding hextree.
-    depth (int): The depth of current hextree.
-    val (float): The padding value. (Default: :obj:`0.0`)
-  '''
+    Args:
+      data (torch.Tensor): The input tensor with its number of elements equal to the
+          non-empty hextree node number.
+      hextree (Hextree): The corresponding hextree.
+      depth (int): The depth of current hextree.
+      val (float): The padding value. (Default: :obj:`0.0`)
+    '''
 
-  mask = hextree.nempty_mask(depth)
-  size = (hextree.nnum[depth], data.shape[1])  # (N, C)
-  out = torch.full(size, val, dtype=data.dtype, device=data.device)
-  out[mask] = data
-  return out
+    mask = hextree.nempty_mask(depth)
+    size = (hextree.nnum[depth], data.shape[1])  # (N, C)
+    out = torch.full(size, val, dtype=data.dtype, device=data.device)
+    out[mask] = data
+    return out
 
 
 def hextree_depad(data: torch.Tensor, hextree: Hextree, depth: int):
-  r''' Reverse operation of :func:`hextree_depad`.
+    r''' Reverse operation of :func:`hextree_depad`.
 
-  Please refer to :func:`hextree_depad` for the meaning of the arguments.
-  '''
+    Please refer to :func:`hextree_depad` for the meaning of the arguments.
+    '''
 
-  mask = hextree.nempty_mask(depth)
-  return data[mask]
+    mask = hextree.nempty_mask(depth)
+    return data[mask]
