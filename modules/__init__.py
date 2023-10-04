50577149bd7670135fda109e6801a8f9bb2988ac
herocat <1722791503@qq.com>
2023-10-04 09:45:55 +0800

    hextree interp

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,12 +1,12 @@
 from .hextree_drop import HextreeDropPath
 from .point_4d_convolution import P4DConv, P4DTransConv
-from .hextree_conv import HextreeConv, HextreeConvBn, HextreeConvBnRelu, HextreeDeconvBnRelu
+from .hextree_conv import HextreeConv, HextreeConvBn, HextreeDeconv, HextreeConvBnRelu, HextreeDeconvBnRelu
 from .hextree_interp import HextreeInterp, HextreeUpsample
 
 __all__ = [
     'HextreeDropPath',
     'P4DConv', 'P4DTransConv',
-    'HextreeConv', 'HextreeConvBn', 'HextreeConvBnRelu', 'HextreeDeconvBnRelu',
+    'HextreeConv', 'HextreeConvBn', 'HextreeDeconv', 'HextreeConvBnRelu', 'HextreeDeconvBnRelu',
     'HextreeInterp', 'HextreeUpsample'
 ]
 
