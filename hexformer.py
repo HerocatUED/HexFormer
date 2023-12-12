b95cca152e8b37d9ea398c8c55b54bacea95a569
herocat <1722791503@qq.com>
2023-12-12 22:28:13 +0800

    RPE2 done

--- hexformer.py
+++ hexformer.py
@@ -63,6 +63,44 @@ class RPE(torch.nn.Module):
     def extra_repr(self) -> str:
         return 'num_heads={}, pos_bnd={}, dilation={}'.format(
                 self.num_heads, self.pos_bnd, self.dilation)  # noqa
+        
+
+class RPE2(torch.nn.Module):
+
+    def __init__(self, patch_size: int, num_heads: int, dilation: int = 1):
+        super().__init__()
+        self.patch_size = patch_size
+        self.num_heads = num_heads
+        self.dilation = dilation
+        self.pos_bnd = self.get_pos_bnd(patch_size)
+        self.rpe_num = 2 * self.pos_bnd + 1
+        self.rpe_table_w = torch.nn.Parameter(torch.zeros(4*self.rpe_num, num_heads))
+        self.rpe_table_b = torch.nn.Parameter(torch.zeros(4*self.rpe_num, num_heads))
+        torch.nn.init.trunc_normal_(self.rpe_table_w, std=0.02)
+        torch.nn.init.trunc_normal_(self.rpe_table_b, std=0.02)
+
+    def get_pos_bnd(self, patch_size: int):
+        return int(0.8 * patch_size * self.dilation**0.5)
+
+    def txyz2idx(self, txyz: torch.Tensor):
+        mul = torch.arange(4, device=txyz.device) * self.rpe_num
+        txyz = txyz.clamp(-self.pos_bnd, self.pos_bnd)
+        idx = txyz + (self.pos_bnd + mul)
+        return idx
+
+    def forward(self, txyz):
+        idx = self.txyz2idx(txyz)
+        w = self.rpe_table_w.index_select(0, idx.reshape(-1))
+        w = w.view(idx.shape + (-1,)).sum(3)
+        w = w.permute(0, 3, 1, 2)  # (N, K, K, H) -> (N, H, K, K)
+        b = self.rpe_table_b.index_select(0, idx.reshape(-1))
+        b = b.view(idx.shape + (-1,)).sum(3)
+        b = b.permute(0, 3, 1, 2)  # (N, K, K, H) -> (N, H, K, K)
+        return w, b
+
+    def extra_repr(self) -> str:
+        return 'num_heads={}, pos_bnd={}, dilation={}'.format(
+                self.num_heads, self.pos_bnd, self.dilation)  # noqa
 
 
 class HextreeAttention(torch.nn.Module):
@@ -84,7 +122,7 @@ class HextreeAttention(torch.nn.Module):
         self.proj = torch.nn.Linear(dim, dim)
         self.proj_drop = torch.nn.Dropout(proj_drop)
         self.softmax = torch.nn.Softmax(dim=-1)
-        self.rpe = RPE(patch_size, num_heads, dilation) if self.use_rpe else None
+        self.rpe = RPE2(patch_size, num_heads, dilation) if self.use_rpe else None
 
     def forward(self, data: torch.Tensor, hextree: HextreeT, depth: int):
         H = self.num_heads
@@ -128,7 +166,13 @@ class HextreeAttention(torch.nn.Module):
 
     def apply_rpe(self, attn, rel_pos):
         if self.use_rpe:
-            attn = attn + self.rpe(rel_pos)
+            if isinstance(self.rpe, RPE2):
+                w, b = self.rpe(rel_pos)
+                attn = w*attn + b
+            elif isinstance(self.rpe, RPE):
+                attn = attn + self.rpe(rel_pos) 
+            else: 
+                assert NotImplementedError, 'only RPE and RPE2 implemented!'
         return attn
 
     def extra_repr(self) -> str:
