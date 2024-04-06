bd957058e6e958289911053374f81cf1bf12c273
herocatued <1722791503@qq.com>
2024-04-06 22:34:02 +0800

    update

--- builder.py
+++ builder.py
@@ -48,13 +48,12 @@ def hexsegformer_small(in_channels, out_channels, init_depth, **kwargs):
 def hexsegformer_toy(in_channels, out_channels, init_depth, **kwargs):
     return HexFormerSeg(
         in_channels, out_channels,
-        channels=[48, 64, 128, 256],
-        num_blocks=[2, 2, 6, 2],
+        channels=[48, 96, 144, 192],
+        num_blocks=[2, 2, 2, 2],
         num_heads=[4, 8, 16, 32],
-        patch_size=64, dilation=4,
+        patch_size=48, dilation=4,
         drop_path=0.3, nempty=True,
         stem_down=2, head_up=2,
-        fpn_channel=128,
         head_drop=[0.5, 0.5], 
         init_depth=init_depth)
     
--- hexformerseg.py
+++ hexformerseg.py
@@ -42,13 +42,13 @@ class HexFormerSeg(torch.nn.Module):
             num_heads: List[int] = [6, 12, 24, 24],
             patch_size: int = 32, dilation: int = 4, drop_path: float = 0.5,
             nempty: bool = True, stem_down: int = 2, head_up: int = 1,
-            fpn_channel: int = 168, head_drop: List[float] = [0.0, 0.0], init_depth: int = 10, **kwargs):
+            head_drop: List[float] = [0.0, 0.0], init_depth: int = 10, **kwargs):
         super().__init__()
         assert stem_down == head_up
         self.backbone = HexFormer(
-            in_channels, channels, num_blocks, num_heads, fpn_channel, 
-            patch_size, dilation, drop_path, nempty, stem_down, init_depth)
-        self.head = SegHeader(out_channels, fpn_channel, nempty, head_up, head_drop)
+            in_channels, channels, num_blocks, num_heads, patch_size, 
+            dilation, drop_path, nempty, stem_down, init_depth)
+        self.head = SegHeader(out_channels, channels[0], nempty, head_up, head_drop)
         self.apply(self.init_weights)
 
     def init_weights(self, m):
